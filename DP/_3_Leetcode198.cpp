2a2fc29532e6171c9ed051c14f3b7f42d33950e4
akshatjaiswal-2003 <akshat.work108@gmail.com>
2025-12-10 20:44:07 +0530

    hello

--- DP/_3_Leetcode198.cpp
+++ DP/_3_Leetcode198.cpp
@@ -1,3 +1,6 @@
+-----------------------------------------Bottom-Up DP approach--------------------------------------------------
+
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -32,5 +35,38 @@ public:
 
         // Last stored value hi hamara final answer hai
         return prev1;
+    }
+};
+
+
+---------------------------------------Top-Down DP (Memoization) approach------------------------------------------
+
+
+class Solution {
+public:
+    int robHelper(int i, vector<int>& nums, vector<int>& memo) {
+        // Base case: agar index < 0 → 0 profit
+        if (i < 0) return 0;
+
+        // Agar already calculate kiya hua hai, return it
+        if (memo[i] != -1) return memo[i];
+
+        // Option 1 → Rob current house
+        int rob = nums[i] + robHelper(i - 2, nums, memo);
+
+        // Option 2 → Skip current house
+        int skip = robHelper(i - 1, nums, memo);
+
+        // Best choice
+        memo[i] = max(rob, skip);
+
+        return memo[i];
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> memo(n, -1);  // DP array for memoization
+
+        // Start from last house
+        return robHelper(n - 1, nums, memo);
     }
 };
